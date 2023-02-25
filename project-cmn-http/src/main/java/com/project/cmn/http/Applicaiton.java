72a7fe6cac1c159bbd1ef96601db8a27e414cd97
nwkim <nwkim1123@gmail.com>
2023-02-25 20:51:35 +0900

    다중 데이터소스 설정 중

--- project-cmn-http/src/main/java/com/project/cmn/http/Applicaiton.java
+++ project-cmn-http/src/main/java/com/project/cmn/http/Applicaiton.java
@@ -1,6 +1,5 @@
 package com.project.cmn.http;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -8,10 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.project.cmn")
-@MapperScan(basePackages = "com.project.cmn.http.persistence.conditioncoupon.mapper", sqlSessionTemplateRef = "conditioncoupon-template", lazyInitialization = "true")
-@MapperScan(basePackages = "com.project.cmn.http.persistence.lmmdev11.mapper", sqlSessionTemplateRef = "lmmdev11-template", lazyInitialization = "true")
 public class Applicaiton extends SpringBootServletInitializer {
-
     /**
      * @param args
      */
